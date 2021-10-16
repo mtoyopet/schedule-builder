bb6e31693655ba3515f7c8d8df683287175c1ab7
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-10-17 00:09:18 +0900

    calenderList working

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+window.calenderList = []
+
+// chrome.runtime.runMessage.addListener(function(request, sender, sendResponse) {
+//   window.calenderList
+// })
+
 chrome.action.onClicked.addListener(function () {
   chrome.tabs.create({ url: 'index.html' })
 })
+
--- oauth.js
+++ oauth.js
@@ -1,7 +1,27 @@
 window.onload = function () {
   document.querySelector('button').addEventListener('click', function() {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
-    });
-  });
-};
+
+      let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+      }
+      fetch(
+        'https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=2021-10-01T10:00:00-07:00',
+        init
+      )
+      .then((response) => response.json())
+      .then(function(data) {
+        // chrome.runtime.sendMessage({
+
+        // })
+        console.log({ data })
+      })
+    })
+  })
+}
